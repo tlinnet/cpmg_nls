e1425338cad4a38f0f3fd131404c082c5c907d0d
Troels E. Linnet <tlinnet@gmail.com>
2013-03-25 22:18:58 +0100

    Added TB files

---
+++ TB.py
@@ -0,0 +1,73 @@
+from pylab import *
+import numpy
+import scipy.optimize
+import os
+
+figsize = 16
+titfont = 26
+labfont = 12
+figfont = 12
+
+def R1r(x,R1,R2):
+    return R1*cos(x*pi/180)**2+R2*sin(x*pi/180)**2
+
+def R1r_exch(x,y,R1,R2,phi,kEX):
+    return R1*cos(x*pi/180)**2+(R2+phi*kEX/((2*pi*y/tan(x*pi/180))**2+(2*pi*y)**2+kEX**2))*sin(x*pi/180)**2
+
+def insdat(dat,dt):
+    p = dat[1]
+    for i in range(len(dat[2])):
+        mf=dat[2][i]
+        for j in range(len(dt)):
+            ft = dt[j]
+            path = os.path.join(p,mf[0],ft[0]+mf[0]+ft[1])
+            if ft[-1] == '.ser':
+                data = genfromtxt(path)
+                datas = genfromtxt(path,usecols=(6),converters = {6: lambda s: str(s)})
+            else: 
+                data = genfromtxt(path)
+                datas=[]
+            dat[2][i][1].append([data,datas])
+    return(dat)
+
+def plotstats(dat,ft):
+    dc = dat[0]
+    figA = figure(figsize=(figsize, figsize/1.618))
+    figA.suptitle('Tada',fontsize=titfont)
+    figA1 = figA.add_subplot(111)
+    for i in range(len(dat[2])):
+        if dat[2][i][0] in ft:
+            NIs= dat[2][i][1][1][0][:,0]
+            datX = 1 - NIs/max(NIs)
+            datY = dat[2][i][1][1][0][:,38]
+            figA1.plot(datX,datY,'.-',label='%s : %s %s %s %s'%(dat[2][i][0], dc[0],dc[1],dc[2],dc[3]))
+    figA1.set_ylim([0,0.01])
+    figA1.legend(loc="upper left")
+    figA1.set_ylabel('Residual stdev',fontsize=labfont)
+    figA1.set_xlabel("Sparseness",fontsize=labfont)
+    show()
+
+def arrdata(dat,ft):
+    collect = []
+    for i in range(len(dat[2])):
+        if dat[2][i][0] in ft:
+            met = dat[2][i][0]
+            NIs = dat[2][i][1][1][0][:,0]
+            NIs = NIs.astype(int)
+            NIsm = max(NIs)
+            peaki = dat[2][i][1][0][0][:,0]
+            peaki = peaki.astype(int)
+            pm = max(peaki)
+            peaks = []
+            intens = []
+            for j in range(pm):
+                peak = peaki[j]
+                peakn = dat[2][i][1][0][1][j]
+                intens = dat[2][i][1][0][0][:,5][j::pm]
+                NIintens = []
+                for k in range(len(NIs)):
+                    NIint = [NIs[k],dat[2][i][1][0][0][:,8+k][j::pm]]
+                    NIintens.append(NIint)
+                peaks.append([peak,peakn,['Full FT int',intens],NIintens])
+            collect.append([met,NIsm,peaks])
+    return(collect)
--- TB_final.py
+++ TB_final.py
@@ -13,11 +13,11 @@ figfont = 12
 
 #Data FT
 hewl_FT = []
-hewl_FT.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal.fid/analysis_FT/int_corr_ft_method_all_awk_full/FT/allplanes_FT.stats'),'r','hewl-75/33','FT',10,50,'y'])
-hewl_FT.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS30_MDD500.fid/analysis_FT/int_corr_ft_method_all_awk_full/FT/allplanes_FT.stats'),'g','hewl-75/33','FT',30,500,'y'])
-hewl_FT.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS100_MDD500.fid/analysis_FT/int_corr_ft_method_all_awk_full/FT/allplanes_FT.stats'),'b','hewl-75/33','FT',100,500,'y'])
-#hewl_FT.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_NoRandom.fid/analysis_FT/int_corr_ft_method_all_awk_full/FT/allplanes_FT.stats'),'c','hewl-75/33','FT',10,50,'n'])
-#hewl_FT.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS30_MDD500_NoRandom.fid/analysis_FT/int_corr_ft_method_all_awk_full/FT/allplanes_FT.stats'),'y','hewl-75/33','FT',30,500,'n'])
+hewl_FT.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal.fid/analysis_FT/int_corr_ft_method_all_awk_full/FT/allplanes_FT.stats'),'r','hewl-19/112','FT',10,50,'y'])
+hewl_FT.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS30_MDD500.fid/analysis_FT/int_corr_ft_method_all_awk_full/FT/allplanes_FT.stats'),'g','hewl-19/112','FT',30,500,'y'])
+hewl_FT.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS100_MDD500.fid/analysis_FT/int_corr_ft_method_all_awk_full/FT/allplanes_FT.stats'),'b','hewl-19/112','FT',100,500,'y'])
+#hewl_FT.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_NoRandom.fid/analysis_FT/int_corr_ft_method_all_awk_full/FT/allplanes_FT.stats'),'c','hewl-19/112','FT',10,50,'n'])
+#hewl_FT.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS30_MDD500_NoRandom.fid/analysis_FT/int_corr_ft_method_all_awk_full/FT/allplanes_FT.stats'),'y','hewl-19/112','FT',30,500,'n'])
 
 acbp_FT = []
 acbp_FT.append([genfromtxt('/home/tlinnet/kte/acbp/acbp_cpmg_disp_04MGuHCl_40C_041223_normal.fid/analysis_FT/int_corr_ft_method_all_awk_full/FT/allplanes_FT.stats'),'r','acbp-17/82','FT',10,50,'y'])
@@ -25,17 +25,17 @@ acbp_FT.append([genfromtxt('/home/tlinnet/kte/acbp/acbp_cpmg_disp_04MGuHCl_40C_0
 acbp_FT.append([genfromtxt('/home/tlinnet/kte/acbp/acbp_cpmg_disp_04MGuHCl_40C_041223_CS100_MDD500.fid/analysis_FT/int_corr_ft_method_all_awk_full/FT/allplanes_FT.stats'),'b','acbp-17/82','FT',100,500,'y'])
 
 bbl_FT = []
-bbl_FT.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_normal/analysis_FT/int_corr_ft_method_all_awk_full/FT/allplanes_FT.stats'),'r','bbl-19/112','FT',10,50,'y'])
-bbl_FT.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_CS30_MDD500/analysis_FT/int_corr_ft_method_all_awk_full/FT/allplanes_FT.stats'),'g','bbl-19/112','FT',30,500,'y'])
-bbl_FT.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_CS100_MDD500/analysis_FT/int_corr_ft_method_all_awk_full/FT/allplanes_FT.stats'),'b','bbl-19/112','FT',100,500,'y'])
+bbl_FT.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_normal/analysis_FT/int_corr_ft_method_all_awk_full/FT/allplanes_FT.stats'),'r','bbl-75/33','FT',10,50,'y'])
+bbl_FT.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_CS30_MDD500/analysis_FT/int_corr_ft_method_all_awk_full/FT/allplanes_FT.stats'),'g','bbl-75/33','FT',30,500,'y'])
+bbl_FT.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_CS100_MDD500/analysis_FT/int_corr_ft_method_all_awk_full/FT/allplanes_FT.stats'),'b','bbl-75/33','FT',100,500,'y'])
 
 #Data MDD
 hewl_MDD = []
-hewl_MDD.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal.fid/analysis_FT/int_corr_ft_method_all_awk_full/MDD/allplanes_MDD.stats'),'r','hewl-75/33','MDD',10,50,'y'])
-hewl_MDD.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS30_MDD500.fid/analysis_FT/int_corr_ft_method_all_awk_full/MDD/allplanes_MDD.stats'),'g','hewl-75/33','MDD',30,500,'y'])
-hewl_MDD.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS100_MDD500.fid/analysis_FT/int_corr_ft_method_all_awk_full/MDD/allplanes_MDD.stats'),'b','hewl-75/33','MDD',100,500,'y'])
-#hewl_MDD.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_NoRandom.fid/analysis_FT/int_corr_ft_method_all_awk_full/MDD/allplanes_MDD.stats'),'c','hewl-75/33','MDD',10,50,'n'])
-#hewl_MDD.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS30_MDD500_NoRandom.fid/analysis_FT/int_corr_ft_method_all_awk_full/MDD/allplanes_MDD.stats'),'y','hewl-75/33','MDD',30,500,'n'])
+hewl_MDD.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal.fid/analysis_FT/int_corr_ft_method_all_awk_full/MDD/allplanes_MDD.stats'),'r','hewl-19/112','MDD',10,50,'y'])
+hewl_MDD.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS30_MDD500.fid/analysis_FT/int_corr_ft_method_all_awk_full/MDD/allplanes_MDD.stats'),'g','hewl-19/112','MDD',30,500,'y'])
+hewl_MDD.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS100_MDD500.fid/analysis_FT/int_corr_ft_method_all_awk_full/MDD/allplanes_MDD.stats'),'b','hewl-19/112','MDD',100,500,'y'])
+#hewl_MDD.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_NoRandom.fid/analysis_FT/int_corr_ft_method_all_awk_full/MDD/allplanes_MDD.stats'),'c','hewl-19/112','MDD',10,50,'n'])
+#hewl_MDD.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS30_MDD500_NoRandom.fid/analysis_FT/int_corr_ft_method_all_awk_full/MDD/allplanes_MDD.stats'),'y','hewl-19/112','MDD',30,500,'n'])
 
 acbp_MDD = []
 acbp_MDD.append([genfromtxt('/home/tlinnet/kte/acbp/acbp_cpmg_disp_04MGuHCl_40C_041223_normal.fid/analysis_FT/int_corr_ft_method_all_awk_full/MDD/allplanes_MDD.stats'),'r','acbp-17/82','MDD',10,50,'y'])
@@ -43,17 +43,17 @@ acbp_MDD.append([genfromtxt('/home/tlinnet/kte/acbp/acbp_cpmg_disp_04MGuHCl_40C_
 acbp_MDD.append([genfromtxt('/home/tlinnet/kte/acbp/acbp_cpmg_disp_04MGuHCl_40C_041223_CS100_MDD500.fid/analysis_FT/int_corr_ft_method_all_awk_full/MDD/allplanes_MDD.stats'),'b','acbp-17/82','MDD',100,500,'y'])
 
 bbl_MDD = []
-bbl_MDD.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_normal/analysis_FT/int_corr_ft_method_all_awk_full/MDD/allplanes_MDD.stats'),'r','bbl-19/112','MDD',10,50,'y'])
-bbl_MDD.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_CS30_MDD500/analysis_FT/int_corr_ft_method_all_awk_full/MDD/allplanes_MDD.stats'),'g','bbl-19/112','MDD',30,500,'y'])
-bbl_MDD.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_CS100_MDD500/analysis_FT/int_corr_ft_method_all_awk_full/MDD/allplanes_MDD.stats'),'b','bbl-19/112','MDD',100,500,'y'])
+bbl_MDD.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_normal/analysis_FT/int_corr_ft_method_all_awk_full/MDD/allplanes_MDD.stats'),'r','bbl-75/33','MDD',10,50,'y'])
+bbl_MDD.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_CS30_MDD500/analysis_FT/int_corr_ft_method_all_awk_full/MDD/allplanes_MDD.stats'),'g','bbl-75/33','MDD',30,500,'y'])
+bbl_MDD.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_CS100_MDD500/analysis_FT/int_corr_ft_method_all_awk_full/MDD/allplanes_MDD.stats'),'b','bbl-75/33','MDD',100,500,'y'])
 
 #Data coMDD
 hewl_coMDD = []
-hewl_coMDD.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal.fid/analysis_FT/int_corr_ft_method_all_awk_full/coMDD/allplanes_coMDD.stats'),'r','hewl-75/33','coMDD',10,50,'y'])
-hewl_coMDD.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS30_MDD500.fid/analysis_FT/int_corr_ft_method_all_awk_full/coMDD/allplanes_coMDD.stats'),'g','hewl-75/33','coMDD',30,500,'y'])
-hewl_coMDD.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS100_MDD500.fid/analysis_FT/int_corr_ft_method_all_awk_full/coMDD/allplanes_coMDD.stats'),'b','hewl-75/33','coMDD',100,500,'y'])
-#hewl_coMDD.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_NoRandom.fid/analysis_FT/int_corr_ft_method_all_awk_full/coMDD/allplanes_coMDD.stats'),'c','hewl-75/33','coMDD',10,50,'n'])
-#hewl_coMDD.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS30_MDD500_NoRandom.fid/analysis_FT/int_corr_ft_method_all_awk_full/coMDD/allplanes_coMDD.stats'),'y','hewl-75/33','coMDD',30,500,'n'])
+hewl_coMDD.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal.fid/analysis_FT/int_corr_ft_method_all_awk_full/coMDD/allplanes_coMDD.stats'),'r','hewl-19/112','coMDD',10,50,'y'])
+hewl_coMDD.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS30_MDD500.fid/analysis_FT/int_corr_ft_method_all_awk_full/coMDD/allplanes_coMDD.stats'),'g','hewl-19/112','coMDD',30,500,'y'])
+hewl_coMDD.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS100_MDD500.fid/analysis_FT/int_corr_ft_method_all_awk_full/coMDD/allplanes_coMDD.stats'),'b','hewl-19/112','coMDD',100,500,'y'])
+#hewl_coMDD.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_NoRandom.fid/analysis_FT/int_corr_ft_method_all_awk_full/coMDD/allplanes_coMDD.stats'),'c','hewl-19/112','coMDD',10,50,'n'])
+#hewl_coMDD.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS30_MDD500_NoRandom.fid/analysis_FT/int_corr_ft_method_all_awk_full/coMDD/allplanes_coMDD.stats'),'y','hewl-19/112','coMDD',30,500,'n'])
 
 acbp_coMDD = []
 acbp_coMDD.append([genfromtxt('/home/tlinnet/kte/acbp/acbp_cpmg_disp_04MGuHCl_40C_041223_normal.fid/analysis_FT/int_corr_ft_method_all_awk_full/coMDD/allplanes_coMDD.stats'),'r','acbp-17/82','coMDD',10,50,'y'])
@@ -61,17 +61,17 @@ acbp_coMDD.append([genfromtxt('/home/tlinnet/kte/acbp/acbp_cpmg_disp_04MGuHCl_40
 acbp_coMDD.append([genfromtxt('/home/tlinnet/kte/acbp/acbp_cpmg_disp_04MGuHCl_40C_041223_CS100_MDD500.fid/analysis_FT/int_corr_ft_method_all_awk_full/coMDD/allplanes_coMDD.stats'),'b','acbp-17/82','coMDD',100,500,'y'])
 
 bbl_coMDD = []
-bbl_coMDD.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_normal/analysis_FT/int_corr_ft_method_all_awk_full/coMDD/allplanes_coMDD.stats'),'r','bbl-19/112','coMDD',10,50,'y'])
-bbl_coMDD.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_CS30_MDD500/analysis_FT/int_corr_ft_method_all_awk_full/coMDD/allplanes_coMDD.stats'),'g','bbl-19/112','coMDD',30,500,'y'])
-bbl_coMDD.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_CS100_MDD500/analysis_FT/int_corr_ft_method_all_awk_full/coMDD/allplanes_coMDD.stats'),'b','bbl-19/112','coMDD',100,500,'y'])
+bbl_coMDD.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_normal/analysis_FT/int_corr_ft_method_all_awk_full/coMDD/allplanes_coMDD.stats'),'r','bbl-75/33','coMDD',10,50,'y'])
+bbl_coMDD.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_CS30_MDD500/analysis_FT/int_corr_ft_method_all_awk_full/coMDD/allplanes_coMDD.stats'),'g','bbl-75/33','coMDD',30,500,'y'])
+bbl_coMDD.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_CS100_MDD500/analysis_FT/int_corr_ft_method_all_awk_full/coMDD/allplanes_coMDD.stats'),'b','bbl-75/33','coMDD',100,500,'y'])
 
 #Data CS
 hewl_CS = []
-hewl_CS.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal.fid/analysis_FT/int_corr_ft_method_all_awk_full/CS/allplanes_CS.stats'),'r','hewl-75/33','CS',10,50,'y'])
-hewl_CS.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS30_MDD500.fid/analysis_FT/int_corr_ft_method_all_awk_full/CS/allplanes_CS.stats'),'g','hewl-75/33','CS',30,500,'y'])
-hewl_CS.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS100_MDD500.fid/analysis_FT/int_corr_ft_method_all_awk_full/CS/allplanes_CS.stats'),'b','hewl-75/33','CS',100,500,'y'])
-#hewl_CS.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_NoRandom.fid/analysis_FT/int_corr_ft_method_all_awk_full/CS/allplanes_CS.stats'),'c','hewl-75/33','CS',10,50,'n'])
-#hewl_CS.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS30_MDD500_NoRandom.fid/analysis_FT/int_corr_ft_method_all_awk_full/CS/allplanes_CS.stats'),'y','hewl-75/33','CS',30,500,'n'])
+hewl_CS.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal.fid/analysis_FT/int_corr_ft_method_all_awk_full/CS/allplanes_CS.stats'),'r','hewl-19/112','CS',10,50,'y'])
+hewl_CS.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS30_MDD500.fid/analysis_FT/int_corr_ft_method_all_awk_full/CS/allplanes_CS.stats'),'g','hewl-19/112','CS',30,500,'y'])
+hewl_CS.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS100_MDD500.fid/analysis_FT/int_corr_ft_method_all_awk_full/CS/allplanes_CS.stats'),'b','hewl-19/112','CS',100,500,'y'])
+#hewl_CS.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_NoRandom.fid/analysis_FT/int_corr_ft_method_all_awk_full/CS/allplanes_CS.stats'),'c','hewl-19/112','CS',10,50,'n'])
+#hewl_CS.append([genfromtxt('/home/tlinnet/kte/080716_cpmgDisp_HEWLpH65_normal_CS30_MDD500_NoRandom.fid/analysis_FT/int_corr_ft_method_all_awk_full/CS/allplanes_CS.stats'),'y','hewl-19/112','CS',30,500,'n'])
 
 acbp_CS = []
 acbp_CS.append([genfromtxt('/home/tlinnet/kte/acbp/acbp_cpmg_disp_04MGuHCl_40C_041223_normal.fid/analysis_FT/int_corr_ft_method_all_awk_full/CS/allplanes_CS.stats'),'r','acbp-17/82','CS',10,50,'y'])
@@ -79,9 +79,9 @@ acbp_CS.append([genfromtxt('/home/tlinnet/kte/acbp/acbp_cpmg_disp_04MGuHCl_40C_0
 acbp_CS.append([genfromtxt('/home/tlinnet/kte/acbp/acbp_cpmg_disp_04MGuHCl_40C_041223_CS100_MDD500.fid/analysis_FT/int_corr_ft_method_all_awk_full/CS/allplanes_CS.stats'),'b','acbp-17/82','CS',100,500,'y'])
 
 bbl_CS = []
-bbl_CS.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_normal/analysis_FT/int_corr_ft_method_all_awk_full/CS/allplanes_CS.stats'),'r','bbl-19/112','CS',10,50,'y'])
-bbl_CS.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_CS30_MDD500/analysis_FT/int_corr_ft_method_all_awk_full/CS/allplanes_CS.stats'),'g','bbl-19/112','CS',30,500,'y'])
-bbl_CS.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_CS100_MDD500/analysis_FT/int_corr_ft_method_all_awk_full/CS/allplanes_CS.stats'),'b','bbl-19/112','CS',100,500,'y'])
+bbl_CS.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_normal/analysis_FT/int_corr_ft_method_all_awk_full/CS/allplanes_CS.stats'),'r','bbl-75/33','CS',10,50,'y'])
+bbl_CS.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_CS30_MDD500/analysis_FT/int_corr_ft_method_all_awk_full/CS/allplanes_CS.stats'),'g','bbl-75/33','CS',30,500,'y'])
+bbl_CS.append([genfromtxt('/home/tlinnet/kte/t1rho/bblM_20130104_pH6_5C_0Murea_CS100_MDD500/analysis_FT/int_corr_ft_method_all_awk_full/CS/allplanes_CS.stats'),'b','bbl-75/33','CS',100,500,'y'])
 
 
 ### Fit function
@@ -125,9 +125,9 @@ def plotdata(D,P):
     for i in range(len(D)):
         figA1.plot(D[i][0][:,1], D[i][0][:,38],'.-',color=D[i][1],linewidth=0.5,label='%s %s %s %s %s'%(D[i][2],D[i][3],D[i][4],D[i][5],D[i][6]))
         #figA1.plot(D[i][0][:,1], fitfunc(P[i][0],D[i][0][:,1]),'-',color=D[i][1],linewidth=0.5,label='fit')
-        figA1.annotate('%3.2f'%(P[i][2]), xy=(P[i][2],P[i][3]),xytext=(P[i][2],i*P[i][3]+4*P[i][3]),arrowprops=dict(facecolor=D[i][1], shrink=0.05, width=2))
+        figA1.annotate('%3.2f'%(P[i][2]), xy=(P[i][2],P[i][3]),xytext=(P[i][2],0.1*P[i][3]+i*0.1*P[i][3]),arrowprops=dict(facecolor=D[i][1], shrink=0.05, width=2))
     #figA1.set_title("Overlay of all experiments",fontsize=labfont)
-    figA1.set_ylim([0,0.15])
+    figA1.set_ylim([0,0.01])
     figA1.legend(loc="upper left")
     figA1.set_ylabel('Residual stdev',fontsize=labfont)
     figA1.set_xlabel("Sparseness",fontsize=labfont)
